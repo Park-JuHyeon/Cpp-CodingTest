d5b740a5068eeec425948746fc91c87c928f85dd
Park-JuHyeon <j-hyeon97@naver.com>
2023-08-16 13:22:41 +0900

    ㅎㅇ

--- Cpp-CodingTest/Cpp-CodingTest/file_pattern.cpp
+++ Cpp-CodingTest/Cpp-CodingTest/file_pattern.cpp
@@ -4,16 +4,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n;
-string s, pa;
-int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		getline(cin, pa);
-		getline(cin, s);
-		for (int j = 0; j < s.size(); j++) {
+int n;			// 테스트케이스의 수
+string s, ori_s, pre, suf;		// 문자열 변수들
 
+int main() {
+	cin >> n;		// 테스트케이스 개수 입력
+	cin >> ori_s;	// 원본(암호화된) 문자열 입력
+
+	int pos = ori_s.find('*');		// ori_s에서 * 위치 찾기
+	pre = ori_s.substr(0, pos);		// *(ori_s[1]) 이전부분 추출
+	suf = ori_s.substr(pos + 1);	// *(ori_s[1]) 이후부분(은 어차피 하나라서 +1) 추출
+
+	for (int i = 0; i < n; i++) {	
+		cin >> s;			// 비교할 문자열 n개 입력
+
+		// pre와 suf의 길이가 s보다 크면 NE 출력
+		if (pre.size() + suf.size() > s.size()) {
+			cout << "NE\n";
+		}
+		else {
+			// pre와 suf부분이 s와 일치하면 DA 출력, 그렇지 않으면 NE 출력
+			if (pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size()))
+				cout << "DA\n";
+			else
+				cout << "NE\n";
 		}
 	}
+	return 0;
 	
 }
--- Cpp-CodingTest/Cpp-CodingTest/rot13.cpp
+++ Cpp-CodingTest/Cpp-CodingTest/rot13.cpp
@@ -6,21 +6,19 @@ using namespace std;
 
 string s;
 int main() {
-	getline(cin, s);	// 입력에 띄어쓰기가 있을때에 cin 대신 getline함수를 사용한다.
+	getline(cin, s);
 	for (int i = 0; i < s.size(); i++) {
-		// 대문자일때 아스키 코드 65 ~ 90
 		if (s[i] >= 65 && s[i] < 97) {
 			if (s[i] + 13 > 90) s[i] = s[i] + 13 - 26;
 			else s[i] = s[i] + 13;
 		}
-		// 소문자 아스키코드 97~122
 		if (s[i] >= 97 && s[i] <= 122) {
 			if (s[i] + 13 > 122) s[i] = s[i] + 13 - 26;
 			else s[i] = s[i] + 13;
 		}
-		cout << s[i];
+
+		cout << s[i] << "";
 	}
-	
 	return 0;
 }
 
